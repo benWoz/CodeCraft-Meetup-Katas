e5830961600efde888fe83e9ff6d7cb928d9db0f
Ben Woznicki <b@benWoz.com>
2012-01-04 21:24:36 -0500

    passing 2nd test w/ refactor

--- 2012_01_04_game_of_life/lib/cell.rb
+++ 2012_01_04_game_of_life/lib/cell.rb
@@ -1,8 +1,17 @@
 class Cell
 
-  def alive?
-    false
+  def initialize number_of_neighbors
+    @number_of_neighbors = number_of_neighbors 
   end
 
+  def alive?
+    viable_neighborhood_size.include? @number_of_neighbors
+  end
+
+  private
+  
+  def viable_neighborhood_size
+    (2..3)
+  end
 
 end
--- 2012_01_04_game_of_life/specs/game_spec.rb
+++ 2012_01_04_game_of_life/specs/game_spec.rb
@@ -3,8 +3,23 @@ require "./lib/cell.rb"
 describe Cell do
   
   it "dies if it has less than two neighbors to hook up with" do
-    first_life = Cell.new(0)
+    first_life = Cell.new(1)
     first_life.alive?.should eql false 
+  end
+
+  it "lives with two neighbors" do
+    first_life = Cell.new(2)
+    first_life.alive?.should eql true
+  end
+
+  it "lives with three neighbors" do
+    first_life = Cell.new(3)
+    first_life.alive?.should eql true
+  end
+
+  it "dies if it has more than three neighors" do
+    first_life = Cell.new(4)
+    first_life.alive?.should eql false
   end
 
 end
